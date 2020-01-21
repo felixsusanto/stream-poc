f2c8e4341144db163d79247914a62e5bd29cb12e
Felix Susanto <felix.susanto2000@gmail.com>
2020-01-21 22:29:34 +0800

    creating generator fn

--- generator.js
+++ generator.js
@@ -1,3 +1,25 @@
 const fs = require("fs");
+const es = require("event-stream");
+const faker = require('faker');
 
-fs.createWriteStream();
+const randomGenerator = () => {
+  const obj = {
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    card: faker.helpers.createCard()
+  };
+  return JSON.stringify(obj);
+};
+
+const rs = es.readable(function (count, callback) {
+  if(count > 250)
+    return this.emit('end')
+  
+  const data = randomGenerator();
+  
+  this.emit('data', `${count ? "\n" : ""}${data}`);
+  callback();
+})
+const ws = fs.createWriteStream("master/junk.jsonl");
+
+rs.pipe(ws);
